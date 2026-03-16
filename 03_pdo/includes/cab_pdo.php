ff379a043b183c7f58d1cbc3a078a6adbb87c364
henryportes880 <20241ctb0100075@estudantes.ifpr.edu.br>
2026-03-16 20:41:11 +0000

    feat: desafio aula05 - catalogo com PDO e MariaDB

---
+++ 03_pdo/includes/cab_pdo.php
@@ -0,0 +1,8 @@
+<?php
+if (!isset($titulo_pagina)) $titulo_pagina = "Catalogo de Tecnologias";
+if (!isset($pagina_atual)) $pagina_atual = "";
+
+$caminho_raiz = '../';
+
+include __DIR__ . '/../../includes/cabecalho.php';
+?>
